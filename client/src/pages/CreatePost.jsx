e132b187acbcf37f92b7ec98c872196e8837c8b0
Vitor Henrique <vitorhenrique091@outlook.com>
2023-02-07 06:00:34 -0300

    Terminando o Front-end e começando o back

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -14,10 +14,19 @@ const CreatePost = () => {
   });
   const [generatingImg, setGeneratingImg] = useState(false);
   const [loading, setLoading] = useState(false);
+
+  const generateImage = () => {};
   
   const handleSubmit = () => {};
-  const handleChange = (e) => {};
-  const handleSurpriseMe = () => {};
+
+  const handleChange = (e) => {
+    setForm({ ...form, [e.target.name]: e.target.value})
+  };
+  
+  const handleSurpriseMe = () => {
+    const randomPrompt = getRandomPrompt(form.prompt);
+    setForm({ ...form, prompt: randomPrompt})
+  };
 
   return (
     <section className='max-w-7x1 mx-auto'>
@@ -68,6 +77,26 @@ const CreatePost = () => {
               <Loader/>
             </div>
           )}
+        </div>
+
+        <div className='mt-5 flex gap-5'>
+          <button
+            type='button'
+            onClick={generateImage}
+            className='text-white bg-green-700 font-medium rounded-md text-sm w-full sm:w-auto px-5 py-2.5 text-center'
+          >
+            {generatingImg ? 'Generating...' : 'Generate'}
+          </button>
+        </div>
+
+        <div className='mt-10'>
+          <p className='mt-2 text-[#666e75] text-[14px]'>Once you have created the image you want, you can share it with others in the community</p>
+          <button
+            type='submit'
+            className='mt-3 text-white bg-[#6469ff] font-mediu rounded-md text-sm w-full sm:w-auto px-5 py-2.5 text center'
+          >
+            {loading ? 'Sharing...' : 'Share with the community'}
+          </button>
         </div>
       </form>
     </section>
